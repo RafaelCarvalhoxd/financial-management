1e6ddc9d3fd861807214cb3ba1da485c98bc66f9
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2026-01-27 22:42:05 -0300

    refactor: change file paths

---
+++ internal/apps/auth/dto.go
@@ -0,0 +1,21 @@
+package auth
+
+type RegisterRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RegisterResponse struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
